a84d6b5267cee478f7c97a7f24c9ae4b5e52fb8c
oeshak <104064271+oeshak@users.noreply.github.com>
2024-02-03 00:55:20 +0100

    Update masked-theme.el

--- masked-theme.el
+++ masked-theme.el
@@ -1,127 +1,158 @@
-;; TODOs:
-;; Drag
-;; Bind comment-or-uncomment globally
+;;; masked-theme.el --- Masked color theme for GNU Emacs.
 
-;; Debug? ;;
-(setq debug-on-error t)
+;; Copyright (C) 2024 oeshak <https://github.com/oeshak>
 
-;; MELPA ;;
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Author: oeshak <>
+;; URL: https://github.com/oeshak/masked-theme
+;; Version: 0.1
 
-(package-initialize)
+;; Permission is hereby granted, free of charge, to any person
+;; obtaining a copy of this software and associated documentation
+;; files (the "Software"), to deal in the Software without
+;; restriction, including without limitation the rights to use, copy,
+;; modify, merge, publish, distribute, sublicense, and/or sell copies
+;; of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
 
-;; Sudo ;;
-(use-package sudo-edit
-  :ensure t)
+;; The above copyright notice and this permission notice shall be
+;; included in all copies or substantial portions of the Software.
 
-;; Look & Feel ;;
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
-(defun transparency (value)
-  "Sets the transparency of the frame window. 0 = transparent, 100 = opaque"
-  (interactive "nTransparency Value 0 - 100 opaque:")
-  (set-frame-parameter nil 'alpha-background value)
-  (add-to-list 'default-frame-alist '(alpha-background . value)))
+;; TODO: magit, isearch, icomplete
 
-(setq mouse-wheel-progressive-speed nil
-      text-scale-mode-step 1.2)
+(deftheme masked ()
+          "Masked theme for GNU Emacs")
 
-(use-package highlight-numbers
-  :ensure t
-  :hook ((prog-mode . highlight-numbers-mode)))
+;; colors with `+c' are lighter; and with `-c' darker
+(let ((masked-bg+2        "#2f2f4d")
+      (masked-bg+1        "#222238")
+      (masked-bg+01       "#0f0f10")
+      (masked-bg+00       "#121212")
+      (masked-bg          "#000010")
+      (masked-bg-alt      "#0e0e0e")
 
-(set-face-attribute 'default nil :font "Iosevka Extended" :height 165 :slant 'normal :weight 'light)
+      (masked-ws          "#121212")
 
-(load-file "~/.emacs.d/masked-theme.el")
+      (masked-red         "#a34443")
+      (masked-green       "#8ba446")
+      (masked-yellow      "#987d3e")
+      (masked-blue        "#496f94")
+      (masked-magenta     "#897399")
+      (masked-cyan        "#518a8a")
 
-(require 'masked-theme)
-(load-theme 'masked t)
+      (masked-fg          "#bbbbbb")
+      (masked-fg-1        "#969696")
+      (masked-fg-2        "#696969")
 
-(use-package powerline
-  :ensure t
-  :config
-  (powerline-default-theme))
+      (masked-blue-alt    "#004daa")
+      (masked-magenta-alt "#c617e6")
 
-(use-package moe-theme
-  :disabled t
-  :ensure t
-  :config
-  (add-hook 'text-scale-mode-hook
-            (lambda () (face-remap--remap-face
-                        'line-number)))
-  (add-hook 'text-scale-mode-hook
-            (lambda () (face-remap--remap-face
-                        'line-number-current-line)))
-  (require 'powerline)
-  (setq moe-theme-modeline-color 'black)
-  (powerline-moe-theme)
-  (load-theme 'moe-dark t))
+      (masked-gold        "#ffd700")
+      (masked-black       "#000000")
+      (masked-white       "#ffffff")
 
-(use-package rainbow-delimiters
-  :disabled t
-  :ensure t
-  :hook ((prog-mode . rainbow-delimiters-mode)
-         (org-mode . rainbow-delimiters-mode)))
+      ;; disable bold/italic (if you want) ['bold <-> 'normal v 'italic <-> 'normal]
+      (bold               'bold)
+      (italic             'italic))
 
-(use-package rainbow-mode
-  :ensure t)
+  (custom-theme-set-faces
+   'masked
 
-;; Keybinding
-(global-set-key (kbd "C-c c") 'compile)
-(global-set-key (kbd "C-c b") 'eval-buffer)
-(global-set-key (kbd "C-c t") 'transparency)
-(global-set-key (kbd "C-c s") 'shell-command)
-(global-set-key (kbd "C-c i")
-                (lambda ()
-                  (interactive)
-                  (eldoc-minibuffer-message "Indenting buffer...")
-                  (indent-region 0 (point-max))
-                  (eldoc-minibuffer-message "Indenting buffer...done")))
+   ;; general
+   `(cursor ((t (:background ,masked-gold))))
+   `(default ((t (:foreground ,masked-fg :background ,masked-bg))))
+   `(fringe ((t (:foreground ,masked-fg :background ,masked-bg+00))))
+   `(minibuffer-prompt ((t (:foreground ,masked-white))))
+   `(region ((t (:foreground ,masked-white :background ,masked-blue-alt))))
+   `(link ((t (:foreground ,masked-magenta-alt :underline t))))
+   `(link-visited ((t (:foreground ,masked-magenta :underline t))))
 
-;; Terminal ;;
-(use-package multi-vterm
-  :ensure t)
+   ;; font-lock
+   `(font-lock-builtin-face ((t (:foreground ,masked-green))))
+   `(font-lock-comment-face ((t (:foreground ,masked-blue :slant ,italic))))
+   `(font-lock-comment-delimiter-face ((t (:foreground ,masked-blue :slant ,italic))))
+   `(font-lock-constant-face ((t (:foreground ,masked-cyan))))
+   `(font-lock-doc-face ((t (:foreground ,masked-green))))
+   `(font-lock-function-name-face ((t (:foreground ,masked-fg))))
+   `(font-lock-keyword-face ((t (:foreground ,masked-yellow :weight ,bold))))
+   `(font-lock-preprocessor-face ((t (:foreground ,masked-magenta))))
+   `(font-lock-string-face ((t (:foreground ,masked-red))))
+   `(font-lock-type-face ((t (:foreground ,masked-magenta))))
+   `(font-lock-variable-name-face ((t (:foreground ,masked-fg))))
+   `(font-lock-warning-face ((t (:foreground ,masked-red))))
+   `(font-lock-negation-char-face ((t (:foreground ,masked-red))))
 
-;; Org ;;
-(add-hook 'org-mode-hook
-          (lambda ()
-            (setq
-             display-line-numbers-type 'normal)
-            (display-line-numbers-mode 1)))
+   ;; mode-line
+   `(mode-line-active ((t (:foreground ,masked-black :background ,masked-yellow :slant ,italic))))
+   `(mode-line-inactive ((t (:foreground ,masked-fg-1 :background ,masked-bg+01 :slant ,italic))))
+   `(mode-line-buffer-id ((t (:slant ,italic))))
 
-;; Programming ;;
-(add-hook 'prog-mode-hook
-          (lambda ()
-            (setq
-             indent-tabs-mode nil
-             c-default-style "bsd"
-             c-basic-offset 4
-             tab-width 4
-             parens-require-spaces nil
-             show-paren-style 'parenthesis
-             whitespace-style '(face tabs spaces tab-mark space-mark trailing indentation empty missing-newline-at-eof))
-            (whitespace-mode 1)
-            (electric-pair-mode 0)
-            (electric-indent-mode 1)
-            (display-line-numbers-mode 1)))
+   ;; line-numbers
+   `(line-number ((t (:inherit default :foreground ,masked-bg+1 :background ,masked-bg-alt))))
+   `(line-number-current-line ((t (:inherit line-number :foreground ,masked-bg+2 :background ,masked-bg-alt :weight ,bold))))
 
-(add-hook 'python-mode-hook (lambda () (setq python-indent-offset 4)))
+   ;; whitespace
+   `(whitespace-space ((t (:foreground ,masked-ws :background ,masked-bg ))))
+   `(whitespace-tab ((t (:foreground ,masked-ws :background ,masked-bg ))))
 
-(use-package company
-  :ensure t
-  :hook ((prog-mode . company-mode)))
+   ;; org
+   `(org-date ((t (:foreground ,masked-blue :background ,masked-bg))))
+   `(org-hide ((t (:foreground ,masked-fg-1 :background ,masked-bg))))
+   `(org-todo ((t (:foreground ,masked-red :background ,masked-bg))))
+   `(org-done ((t (:foreground ,masked-green :background ,masked-bg))))
+   `(org-headline-done ((t (:inherit org-done))))
+   `(org-level-1 ((t (:foreground ,masked-blue :background ,masked-bg))))
+   `(org-level-2 ((t (:foreground ,masked-cyan :background ,masked-bg))))
+   `(org-level-3 ((t (:foreground ,masked-gold :background ,masked-bg))))
+   `(org-level-4 ((t (:foreground ,masked-green :background ,masked-bg))))
+   `(org-level-5 ((t (:foreground ,masked-magenta :background ,masked-bg))))
 
-(use-package slime
-  :ensure t
-  :config (setq inferior-lisp-program "sbcl"))
+   ;; compilation
+   `(compilation-info ((t (:foreground ,masked-green))))
+   `(compilation-warning ((t (:foreground ,masked-yellow))))
+   `(compilation-error ((t (:foreground ,masked-red))))
+   `(compilation-mode-line-fail ((t (:foreground ,masked-red :weight ,bold))))
+   `(compilation-mode-line-exit ((t (:foreground ,masked-green :weight ,bold))))
 
-;; Custom ;;
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+   ;; ansi-term / vterm
+   `(term-color-black ((t (:foreground ,masked-black :background ,masked-black))))
+   `(term-color-red ((t (:foreground ,masked-red :background ,masked-red))))
+   `(term-color-green ((t (:foreground ,masked-green :background ,masked-green))))
+   `(term-color-blue ((t (:foreground ,masked-blue :background ,masked-blue))))
+   `(term-color-yellow ((t (:foreground ,masked-yellow :background ,masked-yellow))))
+   `(term-color-magenta ((t (:foreground ,masked-magenta :background ,masked-magenta))))
+   `(term-color-cyan ((t (:foreground ,masked-cyan :background ,masked-cyan))))
+   `(term-color-white ((t (:foreground ,masked-fg :background ,masked-fg))))
+
+   ;; dired
+   `(dired-directory ((t (:foreground ,masked-blue :weight ,bold))))
+   `(dired-ignored ((t (:foreground ,masked-cyan))))
+
+   ;; powerline
+   `(powerline-active0 ((t (:foreground ,masked-white  :background ,masked-bg+1))))
+   `(powerline-active1 ((t (:foreground ,masked-white :background ,masked-bg-alt))))
+   `(powerline-active2 ((t (:foreground ,masked-white :background ,masked-bg-alt))))
+
+   `(powerline-inactive0 ((t (:foreground ,masked-fg-1  :background ,masked-bg-alt))))
+   `(powerline-inactive1 ((t (:foreground ,masked-fg-1 :background ,masked-bg-alt))))
+   `(powerline-inactive2 ((t (:foreground ,masked-fg-1 :background ,masked-bg-alt))))
+
+   ;; highlight-numbers
+   `(highlight-numbers-number ((t (:foreground ,masked-red))))))
+
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
+(provide-theme 'masked)
+
+;;; masked-theme.el ends here.
